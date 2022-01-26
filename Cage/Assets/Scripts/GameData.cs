c20665b1b5a1519ad5ca8895857adec95795b921
s20281 <80415912+s20281@users.noreply.github.com>
2022-01-26 11:46:54 +0100

    Door, item save. gitignore save

--- Cage/Assets/Scripts/GameData.cs
+++ Cage/Assets/Scripts/GameData.cs
@@ -7,4 +7,10 @@ public class GameData
     public Vector3 playerPosition;
     public bool enemy1Alive;
     public bool enemy2Alive;
+    public bool swordPicked;
+    public bool bookPicked;
+    public bool potionPicked;
+    public bool doorUpOpened;
+    public bool doorLeftOpened;
+    public bool doorRightOpened;
 }
--- Cage/Assets/Scripts/LevelObjects.cs
+++ Cage/Assets/Scripts/LevelObjects.cs
@@ -7,6 +7,13 @@ public class LevelObjects : MonoBehaviour
     public GameObject enemy1;
     public GameObject enemy2;
     public GameObject player;
+    public GameObject doorLeft;
+    public GameObject doorUp;
+    public GameObject doorRight;
+
+    public GameObject sword;
+    public GameObject book;
+    public GameObject potion;
 
     void Start()
     {
@@ -19,6 +26,13 @@ public class LevelObjects : MonoBehaviour
         data.enemy1Alive = enemy1.activeSelf;
         data.enemy2Alive = enemy2.activeSelf;
         data.playerPosition = player.transform.position;
+        data.doorLeftOpened = doorLeft.GetComponent<BoxCollider2D>().isActiveAndEnabled;
+        data.doorRightOpened = doorRight.GetComponent<BoxCollider2D>().isActiveAndEnabled;
+        data.doorUpOpened = doorUp.GetComponent<BoxCollider2D>().isActiveAndEnabled;
+
+        data.swordPicked = sword.activeSelf;
+        data.potionPicked = sword.activeSelf;
+        data.bookPicked = book.activeSelf;
     }
     public void LoadObjects(GameData data)
     {
@@ -26,5 +40,14 @@ public class LevelObjects : MonoBehaviour
         player.transform.position = data.playerPosition;
         enemy1.SetActive(data.enemy1Alive);
         enemy2.SetActive(data.enemy2Alive);
+        doorUp.SetActive(data.doorUpOpened);
+        doorLeft.SetActive(data.doorLeftOpened);
+        doorRight.SetActive(data.doorRightOpened);
+
+        sword.SetActive(data.swordPicked);
+        potion.SetActive(data.potionPicked);
+        book.SetActive(data.bookPicked);
+
+        // trzeba zrobiæ ¿eby siê dodawa³y do inventory
     }
 }
